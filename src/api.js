7b8b6c23b8d816d3294b53066fab89e06780528e
Jonas Jensen <jonas.jensen@greenbird.com>
2018-12-29 21:40:24 -0600

    fix husky pre-commit hook

--- src/api.js
+++ src/api.js
@@ -1,24 +1,24 @@
-const { join } = require('path');
-const { promisify } = require('util');
-const { exec } = require('child_process');
-const { createWriteStream } = require('fs');
-const { writeFile } = require('gitignore');
-const { run } = require('npm-check-updates');
-const cpy = require('cpy');
+const { join } = require("path");
+const { promisify } = require("util");
+const { exec } = require("child_process");
+const { createWriteStream } = require("fs");
+const { writeFile } = require("gitignore");
+const { run } = require("npm-check-updates");
+const cpy = require("cpy");
 
 const execP = promisify(exec);
 const writeFileP = promisify(writeFile);
 
 const copyFiles = async directoryName => {
   const targetPath = join(process.cwd(), directoryName);
-  const cwd = join(__dirname, '..', 'resources');
-  await cpy('**/*', targetPath, { cwd, parents: true });
+  const cwd = join(__dirname, "..", "resources");
+  await cpy("**/*", targetPath, { cwd, parents: true });
 
   return targetPath;
 };
 
 const gitInit = async () => {
-  const { stdout, stderr } = await execP('git init');
+  const { stdout, stderr } = await execP("git init");
   if (stderr) {
     throw new Error(stderr);
   }
@@ -30,7 +30,7 @@ const upgradePackageJson = async packageJsonPath =>
   run({ packageFile: packageJsonPath, upgrade: true, upgradeAll: true });
 
 const createGitIgnore = async (fileDirectory, types) => {
-  const gitIgnorePath = join(fileDirectory, '.gitignore');
+  const gitIgnorePath = join(fileDirectory, ".gitignore");
   const promises = types.map(type => {
     const file = createWriteStream(gitIgnorePath, { flags: "a" });
     return writeFileP({ type, file });
@@ -43,5 +43,5 @@ module.exports = {
   copyFiles,
   gitInit,
   upgradePackageJson,
-  createGitIgnore,
+  createGitIgnore
 };
